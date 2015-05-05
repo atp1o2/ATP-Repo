0e1246c2c2b5fc79ac28fcfe51511130e4e979e9
atp1o2 <andrewpham8@gmail.com>
2015-05-05 15:52:27 -0700

    Last test message

--- test.rb
+++ test.rb
@@ -3,3 +3,5 @@ steps for syncing git bundles from desktop to laptop
 Trying to sync with desktop without dropbox
 
 Comment from laptop. Check desktop file, not the one in the dropbox
+
+Don't need dropbox, last test!
